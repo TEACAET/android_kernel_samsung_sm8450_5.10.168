2c1d4f71ebcb355274990d6fca0e8d4c7a3fa90d
TEACAET <1789974726@qq.com>
2024-04-24 13:31:27 +0800

    cpuset: Parameter modification

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2506,10 +2506,10 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 		{ "system-background",	"0-3" },
 		{ "camera-background",	"0-3" },
 		{ "audio-app",		"0,1,2,3" },
-		{ "foreground",		"2,3,4,5,7," },
+		{ "foreground",		"0,1,2,3,4,5,7" },
 		{ "camera-daemon",	"0-7" },
-		{ "top-app",		"2-7" },
-		{ "ui",		        "5-7" },
+		{ "top-app",		"0-7" },
+		{ "ui",		        "4-7" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
